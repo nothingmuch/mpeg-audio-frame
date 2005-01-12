eb8261e9e9731ffb5ab66ffa7218ec6b7f453cc5
Yuval Kogman <nothingmuch@woobling.org>
2005-01-12 23:43:47 +0000

    test dir rehaul

--- t/lib/Test/FloatNear.pm
+++ t/lib/Test/FloatNear.pm
@@ -1,22 +1,18 @@
 #!/usr/bin/perl -w
 
+package Test::FloatNear;
+
 use strict;
 
 use base qw/Exporter/;
 use vars qw/@EXPORT/;
 
-@EXPORT = qw/is_near sum/;
+@EXPORT = qw/is_near/;
 
 sub is_near ($$;@) {
 	my ($got, $expected) = (shift, shift);
 	@_ = (abs($got - $expected), "<", 0.001, @_);
 	goto \&Test::More::cmp_ok;
-}
-
-sub sum {
-	my $s = 0;
-	$s += shift while @_;
-	$s;
 }
 
 __PACKAGE__
